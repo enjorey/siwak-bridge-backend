95cdd0ae7d7e72d8811ed9118ccdc97048a1061d
Siwak <siwakenjorey-bridge@zohomail.com>
2025-11-26 07:24:56 +0300

    Update db.js

--- src/src/db.js
+++ src/src/db.js
@@ -1,1 +1,11 @@
-// database connection placeholder
+const { Pool } = require('pg');
+const { DATABASE_URL } = require('./config');
+
+const pool = new Pool({
+  connectionString: DATABASE_URL,
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+  pool
+};
